2f69e26d352c0269fb3052a8ebe3eb596c236f5b
Sergio O <socampo3@gmail.com>
2025-05-06 13:40:30 -0700

    We did it professor!

--- download_data.R
+++ download_data.R
@@ -2,8 +2,10 @@
 # Sergio O.
 # April 22, 2025
 # socampo3@dons.usfca.edu
+
 # Load neonUtilities package
 library("neonUtilities")
+
 # -------------------------------
 # Root biomass and chemistry data
 # -------------------------------
@@ -11,14 +13,17 @@ library("neonUtilities")
 megapit_data <- loadByProduct(dpID = "DP1.10066.001",
                               site = c("DEJU"),
                               check.size = FALSE)
+
 # Extract biomass and carbon-nitrogen data frames
 megapit_biomass <- megapit_data[["mpr_perrootsample"]][["incrementRootBiomass"]]
 megapit_cn <- megapit_data[["mpr_carbonNitrogen"]]
+
 # Save to CSV
 write.csv(megapit_biomass, "data/raw_data/megapit_biomass.csv",
           row.names = FALSE)
 write.csv(megapit_cn, "data/raw_data/megapit_carbon_nitrogen.csv",
           row.names = FALSE)
+
 # Extract and save root sample metadata (used for sizeCategory)
 root_sample <- megapit_data[["mpr_perrootsample"]]
 if (nrow(root_sample) > 0) {
@@ -32,6 +37,7 @@ if (nrow(root_sample) > 0) {
             "data/raw_data/megapit_root_samples.csv",
             row.names = FALSE)
 }
+
 # -------------------------------
 # Soil bulk density data
 # -------------------------------
@@ -39,8 +45,10 @@ if (nrow(root_sample) > 0) {
 soil_data <- loadByProduct(dpID = "DP1.00096.001",
                            site = c("DEJU"),
                            check.size = FALSE)
+
 # Extract relevant table
 soil_bulk <- soil_data[["mgp_perbulksample"]]
+
 # Save to CSV
 write.csv(soil_bulk, "data/raw_data/soil_bulk_density.csv",
           row.names = FALSE)
